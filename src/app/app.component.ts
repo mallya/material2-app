ab7563d50e7b1ea157e1ff7db7a00eb5f3c9ae77
Sunil Mallya <ssmallya@gmail.com>
2017-05-28 21:58:45 -0500

    update to latest version
    ups

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, Optional} from '@angular/core';
 import {MdDialog, MdDialogRef, MdSnackBar} from '@angular/material';
 
 
+
 @Component({
   selector: 'material2-app-app',
   templateUrl: 'app.component.html',
